86d6ae79f79c266fe80eb47396467db9c117200f
Koen Kleifgen <2135272@jeffcoschools.us>
2024-09-05 13:13:20 -0600

    i commit

--- line_count.py
+++ line_count.py
@@ -1,12 +1,11 @@
 def line_count():
   #opens the file as only read
-	file = open('file.txt', 'r')
+  file = open('file.txt', 'r')
   #reads all of the lines in the opened file
-	lines = file.readlines()
+  lines = file.readlines()
   #the length of the lines becomes the output
   output = len(lines)
   #closes the file
   file.close()
   # returnes the output
   return output  
-
